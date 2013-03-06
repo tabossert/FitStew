9b78b427a93573ad658194fb08e81734a535d41e
Trevor Bossert <trevor.bossert@fitstew.com>
2013-03-05 17:24:08 -0800

    Adding Beta copy which is a work in progress from the Design

---
+++ Beta/assets/js/myPanel.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('#classesBlock').hide();
+	$('#partnerBlock').hide();
+	$('#sunday').css("border", "0");
+
+
+	$('#modalBut').click(
+   		function() {
+	   		$('#accountModal').modal('show');
+   		}
+   )
+});
