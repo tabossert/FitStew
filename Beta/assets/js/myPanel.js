ee1fc9da4083824bd06af00c10becba3a3455dec
Trevor Bossert <trevor.bossert@fitstew.com>
2013-03-07 16:43:09 -0800

    more design element work to get things to match psd's.    Now onto API integration

--- Beta/assets/js/myPanel.js
+++ Beta/assets/js/myPanel.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-	$('#classesBlock').hide();
-	$('#partnerBlock').hide();
+	/*$('#classesBlock').hide();
+	$('#partnerBlock').hide();*/
 	$('#sunday').css("border", "0");
 
 
@@ -9,4 +9,35 @@ $(document).ready(function(){
 	   		$('#accountModal').modal('show');
    		}
    )
+
+   function mapLoc(addr) {
+      map = new GMaps({
+        div: '#map',
+        zoom: 16,
+        lat: -12.043333,
+        lng: -77.028333,
+        width: 200,
+        height: 100
+      });
+   
+   
+	   GMaps.geocode({
+		  address: addr,
+		  callback: function(results, status) {
+		    if (status == 'OK') {
+		      var latlng = results[0].geometry.location;
+		      map.setCenter(latlng.lat(), latlng.lng());
+		      map.addMarker({
+		        lat: latlng.lat(),
+		        lng: latlng.lng()
+		      });
+		      map.refresh();
+		    }
+		  }
+	   });
+	}
+
+	mapLoc('1461 Creekside Dr., Walnut Creek, CA');
+
+
 });
